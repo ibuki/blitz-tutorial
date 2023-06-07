2d4e16c4a7663eb828515c3936a02bfd9882521f
Ibuki Suzuki <ibuki.nakamura@gmail.com>
2023-06-07 23:56:53 +0900

    Add question and choice

---
+++ src/questions/mutations/deleteQuestion.ts
@@ -0,0 +1,14 @@
+import { resolver } from "@blitzjs/rpc"
+import db from "db"
+import { DeleteQuestionSchema } from "../schemas"
+
+export default resolver.pipe(
+  resolver.zod(DeleteQuestionSchema),
+  resolver.authorize(),
+  async ({ id }) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const question = await db.question.deleteMany({ where: { id } })
+
+    return question
+  }
+)
